87b986669c88a08270a51c038b4b1a1a2c2deedf
Brayan Gonzalez <brayans.gonzalezd@utadeo.edu.co>
2019-11-13 12:26:58 -0500

    Version 2 autentificacion y registro de usuarios

--- app/src/main/java/com/example/controlcorrect/MainActivity.java
+++ app/src/main/java/com/example/controlcorrect/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.controlcorrect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button inicio, registrar;
+    EditText textCorreo, textContra;
+
+    //Variables de datos a registrar
+    String correo = "";
+    String pass = "";
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        textCorreo = (EditText)findViewById(R.id.correo);
+        textContra = (EditText)findViewById(R.id.password);
+
+        inicio = (Button)findViewById(R.id.botonIniciar);
+        registrar = (Button)findViewById(R.id.botonRegistrar);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registrar = new Intent(MainActivity.this, Registrar.class);
+                startActivity(registrar);
+            }
+        });
+
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                correo = textCorreo.getText().toString();
+                pass = textContra.getText().toString();
+
+                if(!correo.isEmpty() && !pass.isEmpty()){
+                    loginUser();
+                }
+            }
+        });
+    }
+
+    private void loginUser(){
+        mAuth.signInWithEmailAndPassword(correo, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent iniciarSesion = new Intent(MainActivity.this, Principal.class);
+                    startActivity(iniciarSesion);
+                    finish();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "No se pudo iniciar sesion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
